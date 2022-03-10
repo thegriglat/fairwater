f956bbf1fc603819cc7e8c0150a3de4d389ff8a6
Grigory Latyshev <thegriglat@gmail.com>
2022-03-11 02:10:20 +0300

    Connect and resolve names

--- client.cc
+++ client.cc
@@ -1,15 +1,23 @@
 
 #include <iostream>
 #include "network.hh"
+#include <string>
 
-#define N 30
+#define N 300
 
-int main()
+int main(int argc, char **argv)
 {
     char buf[N];
-    SocketNet sckt("127.0.0.1", 22);
+    const std::string host = "cpptest.08z.ru";
+    const unsigned int port = 12567;
+    std::string txt = "asd";
+
+    SocketNet sckt(host, port);
+
     sckt.readData(buf, N);
     buf[N - 1] = '\0';
     std::cout << buf << std::endl;
+    // sckt.sendData((void *)txt.c_str(), txt.size());
+
     return 0;
 }
--- network.cc
+++ network.cc
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <sys/types.h>
 #include <string>
 #include <iostream>
@@ -11,8 +12,32 @@
 
 #define IP_PROTOCOL (0)
 
-SocketNet::SocketNet(const std::string hostip, const unsigned int port)
+namespace
 {
+
+    std::string getIPByName(const std::string &hostname)
+    {
+        struct hostent *he;
+        struct in_addr **addr_list;
+        int i;
+
+        if ((he = gethostbyname(hostname.c_str())) == NULL)
+        {
+            // get the host info
+            std::cerr << "Cannot get IP for " << hostname << std::endl;
+            return "";
+        }
+
+        addr_list = (struct in_addr **)he->h_addr_list;
+
+        std::string ip = inet_ntoa(*addr_list[0]);
+        return ip;
+    }
+};
+
+SocketNet::SocketNet(const std::string hostname, const unsigned int port)
+{
+    std::cout << "Connecting to " << hostname << ":" << port << std::endl;
     socket_fd = socket(AF_INET, SOCK_STREAM, IP_PROTOCOL);
     if (socket_fd < 0)
     {
@@ -23,8 +48,9 @@ SocketNet::SocketNet(const std::string hostip, const unsigned int port)
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
 
-    // TODO: hostname is IP
-    if (inet_pton(AF_INET, hostip.c_str(), &address.sin_addr) <= 0)
+    // TODO: hostname is IP !!!
+    std::cout << getIPByName(hostname) << std::endl;
+    if (inet_pton(AF_INET, getIPByName(hostname).c_str(), &address.sin_addr) <= 0)
     {
         std::cerr << "inet_pton failed" << std::endl;
         exit(1);
@@ -34,6 +60,7 @@ SocketNet::SocketNet(const std::string hostip, const unsigned int port)
         std::cerr << "Socket connect failed!" << std::endl;
         exit(1);
     }
+    std::cout << "Connected" << std::endl;
 };
 
 SocketNet::~SocketNet()
@@ -44,11 +71,20 @@ SocketNet::~SocketNet()
 int SocketNet::readData(void *buffer, size_t size)
 {
     // memset(buffer, '0', size);
-    return read(socket_fd, buffer, size - 1);
+    const int r = read(socket_fd, buffer, size - 1);
+    if (r < 0)
+    {
+        std::cerr << "Cannot read data from socket" << std::endl;
+    }
+    return r;
 }
 
 int SocketNet::sendData(void *buffer, size_t size)
 {
-    // memset(buffer, '0', size);
-    return read(socket_fd, buffer, size - 1);
+    const int r = send(socket_fd, buffer, size - 1, 0);
+    if (r < 0)
+    {
+        std::cerr << "Cannot send data to socket" << std::endl;
+    }
+    return r;
 }
